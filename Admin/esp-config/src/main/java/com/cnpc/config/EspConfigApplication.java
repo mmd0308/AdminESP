b624859b4bca69165de415595fd334288dbc6536
billjiang <475572229@qq.com>
2017-09-19 16:26:07 +0800

    initial commit

---
+++ Admin/esp-config/src/main/java/com/cnpc/config/EspConfigApplication.java
@@ -0,0 +1,12 @@
+package com.cnpc.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspConfigApplication.class, args);
+	}
+}
