253b9d720cefc4128a9392dc6657e5214889dac3
billjiang <475572229@qq.com>
2017-09-20 18:03:14 +0800

    add esp-gateway

--- Admin/esp-config/src/main/java/com/cnpc/config/ConfigApplication.java
+++ Admin/esp-config/src/main/java/com/cnpc/config/ConfigApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EspConfigApplication {
+public class ConfigApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EspConfigApplication.class, args);
+		SpringApplication.run(ConfigApplication.class, args);
 	}
 }
--- Admin/esp-track/src/test/java/com/cnpc/track/TrackApplicationTests.java
+++ Admin/esp-track/src/test/java/com/cnpc/track/TrackApplicationTests.java
@@ -1,4 +1,4 @@
-package com.cnpc.config;
+package com.cnpc.track;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class EspConfigApplicationTests {
+public class TrackApplicationTests {
 
 	@Test
 	public void contextLoads() {
