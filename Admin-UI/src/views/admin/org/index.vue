63522704dd2824a6ae1dcf4f0326362842401908
_hzqing <_hzqing@163.com>
2017-10-23 10:56:49 +0800

    修改组织机构的样式

--- Admin-UI/src/views/admin/org/index.vue
+++ Admin-UI/src/views/admin/org/index.vue
@@ -1,30 +1,31 @@
 <template>
   <div id="org" class="app-container calendar-list-container">
     <el-row  v-loading.body="listLoading">
-      <el-col :span="4">
+      <el-col :span="6">
         <el-input placeholder="输入关键字进行过滤" v-model="filterText">
         </el-input>
-        <el-tree class="filter-tree on-border" :data="data2" :props="defaultProps" :default-expand-all="true"  :filter-node-method="filterNode" :default-expanded-keys="[0,1]" ref="tree2" @node-click="clickTree" @node-expand="expandTree">
+        <el-tree class="filter-tree" style="margin-top:10px;"  :data="data2" :props="defaultProps" :default-expand-all="true"  :filter-node-method="filterNode" :default-expanded-keys="[0,1]" ref="tree2" @node-click="clickTree" @node-expand="expandTree">
         </el-tree>
       </el-col>
-      <el-col :span="20">
+      <el-col :span="18">
         <div class="right-layout-from">
           <div v-if="this.state == 'see'" class="top-button">
-            <el-button type="primary" size="small" native-type="submit"  @click="toCreate()" icon="plus">新增下级机构</el-button>
-            <el-button type="primary" size="small" @click="toUpdate()" icon="edit">编辑当前机构</el-button>
-            <el-button type="danger" size="small" @click="toDeleted()" icon="delete">删除</el-button>
-            <el-button type="primary" size="small" @click="resetOrg('orgFrom')" hidden>重置</el-button>
+            <el-button-group>
+              <el-button type="primary" native-type="submit"  @click="toCreate()" icon="plus">新增下级机构</el-button>
+              <el-button type="primary"  @click="toUpdate()" icon="edit">编辑当前机构</el-button>
+              <el-button type="danger"  @click="toDeleted()" icon="delete">删除</el-button>
+            </el-button-group>
           </div>
           <div  class="top-button">
-            <el-button v-if="this.state == 'add'" type="primary" size="small" native-type="submit"  @click="createOrg('orgFrom')">保存</el-button>
-            <el-button v-if="this.state == 'edit'" type="primary" size="small" native-type="submit"  @click="updateOrg('orgFrom')">保存</el-button>
+            <el-button v-if="this.state == 'add'" type="primary" native-type="submit"  @click="createOrg('orgFrom')">保存</el-button>
+            <el-button v-if="this.state == 'edit'" type="primary" native-type="submit"  @click="updateOrg('orgFrom')">保存</el-button>
           </div>
-          <div class="tree-right-from">
+          <el-card class="box-card" style="margin-bottom: 10px;">
             <el-form :model="orgFrom" :rules="rules" ref="orgFrom" label-width="120px" class="demo-ruleForm" >
               <el-row aria-disabled="">
                 <el-col :span="11">
                   <el-form-item  label="组织机构名称" prop="name">
-                    <el-input v-model="orgFrom.name" v-if="this.state == 'see'" :readonly="true" ></el-input>
+                    <el-input v-model="orgFrom.name" v-if="this.state == 'see'" disabled="disabled" ></el-input>
                     <el-input v-model="orgFrom.name" v-else ></el-input>
                   </el-form-item>
                 </el-col>
@@ -33,7 +34,7 @@
                 </el-col>
                 <el-col :span="11">
                   <el-form-item label="组织机构编码" prop="code">
-                    <el-input v-model="orgFrom.code" v-if="this.state == 'see'" :readonly="true" ></el-input>
+                    <el-input v-model="orgFrom.code" v-if="this.state == 'see'" disabled="disabled" ></el-input>
                     <el-input v-model="orgFrom.code" v-else></el-input>
                   </el-form-item>
                 </el-col>
@@ -41,7 +42,7 @@
               <el-row>
                 <el-col :span="11">
                   <el-form-item label="上级组织机构" prop="parentname">
-                    <el-input v-model="orgFrom.parentname" :readonly="true"></el-input>
+                    <el-input v-model="orgFrom.parentname" disabled="disabled"></el-input>
                   </el-form-item>
                 </el-col>
                 <el-col :span="2">
@@ -65,14 +66,14 @@
                 </el-col>
               </el-row>
               <el-form-item label="层级编码" prop="levelCode">
-                <el-input v-model="orgFrom.levelCode" :readonly="true"></el-input>
+                <el-input v-model="orgFrom.levelCode" disabled="disabled"></el-input>
               </el-form-item>
               <el-form-item label="机构说明" prop="note">
-                <el-input type="textarea" v-model="orgFrom.note" v-if="this.state == 'see'" :readonly="true"></el-input>
+                <el-input type="textarea" v-model="orgFrom.note" v-if="this.state == 'see'" disabled="disabled"></el-input>
                 <el-input type="textarea" v-model="orgFrom.note" v-else></el-input>
               </el-form-item>
             </el-form>
-          </div>
+          </el-card>
         </div>
       </el-col>
     </el-row>
@@ -271,16 +272,6 @@
           }, {
             id: 6,
             label: '二级 2-2'
-          }]
-        }, {
-          id: 3,
-          label: '一级 3',
-          children: [{
-            id: 7,
-            label: '二级 3-1'
-          }, {
-            id: 8,
-            label: '二级 3-2'
           }]
         }],
         defaultProps: {
